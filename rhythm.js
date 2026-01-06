1dc344698cbeeeddb3c797b62155fd3db8e80fc2
InfiniteDigitalArt <69279920+InfiniteDigitalArt@users.noreply.github.com>
2026-01-06 11:00:20 +0000

    Create rhythm.js

---
+++ rhythm.js
@@ -0,0 +1,18 @@
+const RHYTHM_SETS = {
+    none: {
+        chords: [16],
+        bass:   [16]
+    },
+    house: {
+        chords: [4, 4, 4, 4],
+        bass:   [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+    },
+    trance: {
+        chords: [2, 2, 2, 2],
+        bass:   [1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    syncopated: {
+        chords: [1, 0, 1, 1, 0, 1],
+        bass:   [0, 1, 0, 1, 1, 0]
+    }
+};
